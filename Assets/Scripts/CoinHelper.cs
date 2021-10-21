a52f531c1154c7f010dda6ac7de45d920b1629af
Borisov Alexandr <borisov.av.dev@gmail.com>
2021-10-21 16:45:58 +0300

    coins

---
+++ Assets/Scripts/CoinHelper.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CoinHelper : MonoBehaviour
+{
+    private const string PlayerTag = "Player";
+
+    public PlayerController playerController;
+
+    private Vector3 _rotationAngles;
+
+    private void Start()
+    {
+        _rotationAngles = new Vector3(Random.Range(-60, 60), Random.Range(-60, 60), Random.Range(-60, 60));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(PlayerTag))
+        {
+            playerController.PickUpCoin();
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        transform.Rotate(_rotationAngles * Time.deltaTime);
+    }
+}
